c2f97ad40e24f1c9cb4e410a18f78fdd0a5a00d2
zeroxys <miguelzavalac@gmail.com>
2017-08-03 03:29:43 -0500

    working with items

--- resources/assets/js/components/button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Button = (props) => {
-  return (
-    <h1>Quiero ser un boton</h1>
-  )
-}
--- resources/assets/js/components/category/items.js
+++ resources/assets/js/components/category/items.js
@@ -1,15 +1,11 @@
 import React from 'react'
+import HDD from '../../../img/discoduro.png'
 
-export class Items extends React.Component {
-  constructor () {
-    super()
-  }
-
-  render () {
-    return (
-      <div className="Items">
-        <h1>Aqui va a ir cada elemento</h1>
-      </div>
-    )
-  }
+export const Items = () => {
+  return (
+    <div className="itemBox">
+      <img src={HDD} alt="HardDisk" className="itemBox-img"/>
+      <h3>Nombre del producto</h3>
+    </div>
+  )
 }
