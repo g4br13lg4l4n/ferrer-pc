ceaabbb0bc2cd2b65b6acbdc3970fa6b23a1ce0e
Gabriel Galan Mendez <gabrielgalanmendez@MacBook-Air-de-Gabriel-2.local>
2017-08-14 16:37:43 -0500

    replase file web.php of routers

--- routes/web.php
+++ routes/web.php
@@ -10,39 +10,43 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Route::get('/', 'GetProductoController@getProducto');
 
-Route::get('/', function () {
+Route::get('/',function () {
     return view('welcome');
 });
+
+Route::get('/api/productos', 'GetProductoController@getProducto');
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->group(function() {
-    //muestra el formulario de login
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('administrator.login');
-    //envía el formulario de login 
     Route::post('/login', 'Auth\AdminLoginController@login')->name('administrator.login.submit');
 
     Route::get('/', 'AdministratorController@index')->name('administrator.dashboard');
     Route::get('/products', 'AdministratorController@editProduct')->name('administrator.editProduct');
     Route::post('/upexcel','UpexcelController@upExcel');
     
-    //crud 
+    //crud Productos
     Route::post('/addProduct','AdministratorController@addProduct')->name('administrator.addProduct');
     Route::get('/getProduct/{product}', 'AdministratorController@getProduct')->name('administrator.getProduct');
     Route::get('/editProduct/{product}', 'AdministratorController@editProduct')->name('administrator.editProduct');
     Route::post('/updateProduct/{product}', 'AdministratorController@updateProduct')->name('administrator.updateProduct');
-
-    Route::post('/search', 'AdministratorController@searchP')->name('administrator.searchP');
-
-
-
     Route::get('/delete/{product}', 'AdministratorController@deleteProduct')->name('administrator.deleteProduct');
 
+    Route::post('/search', 'AdministratorController@searchP')->name('administrator.searchP');
+    
+    // Marca
     Route::post('/addBrand', 'BrandController@addBrand')->name('administrator.addBrand');
     Route::get('/deleteBrand/{brand}','BrandController@deleteBrand')->name('administrator.deleteBrand');
+
+    //Categoría
+    Route::post('/addCategory','CategoryController@addCategory')->name('administrator.addCategory');
+    Route::get('/delateCategory/{category}','CategoryController@delateCategory')->name('administrator.dalateCategory');
 
 });
 
